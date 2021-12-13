b6c2edfa1e51dcdaf9fa56c48ee4c18f74b871bb
psilin <silinp@yandex.ru>
2021-12-13 18:11:38 +0100

    chore: 10 2/2

--- 2021/10/main.go
+++ 2021/10/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 )
 
 func get_input(path string) []string {
@@ -25,8 +26,10 @@ func get_input(path string) []string {
 func main() {
 	res := get_input("./input.txt")
 	tot_cnt := map[rune]int{'}': 0, '>': 0, ')': 0, ']': 0}
+	comp_slice := []int{}
 	for _, line := range res {
 		stack := []rune{}
+		corrupted := false
 		for _, c := range line {
 			if c == '{' {
 				stack = append(stack, c)
@@ -39,12 +42,14 @@ func main() {
 			} else if c == '}' {
 				if len(stack) == 0 || stack[len(stack)-1] != '{' {
 					tot_cnt[c] += 1
+					corrupted = true
 					break
 				}
 				stack = stack[:len(stack)-1]
 			} else if c == ']' {
 				if len(stack) == 0 || stack[len(stack)-1] != '[' {
 					tot_cnt[c] += 1
+					corrupted = true
 					break
 				}
 				stack = stack[:len(stack)-1]
@@ -52,19 +57,42 @@ func main() {
 			} else if c == '>' {
 				if len(stack) == 0 || stack[len(stack)-1] != '<' {
 					tot_cnt[c] += 1
+					corrupted = true
 					break
 				}
 				stack = stack[:len(stack)-1]
 			} else if c == ')' {
 				if len(stack) == 0 || stack[len(stack)-1] != '(' {
 					tot_cnt[c] += 1
+					corrupted = true
 					break
 				}
 				stack = stack[:len(stack)-1]
 			}
 		}
+		if corrupted == false {
+			cnt := 0
+			for i := len(stack) - 1; i >= 0; i-- {
+				r := 0
+				if stack[i] == '(' {
+					r = 1
+				} else if stack[i] == '[' {
+					r = 2
+				} else if stack[i] == '{' {
+					r = 3
+				} else if stack[i] == '<' {
+					r = 4
+				}
+				cnt = 5*cnt + r
+			}
+			comp_slice = append(comp_slice, cnt)
+		}
 	}
 
 	fmt.Printf("%v\n", tot_cnt)
 	fmt.Printf("%v\n", 3*tot_cnt[')']+57*tot_cnt[']']+1197*tot_cnt['}']+25137*tot_cnt['>'])
+
+	sort.Ints(comp_slice)
+	fmt.Printf("%v\n", comp_slice)
+	fmt.Printf("%v %v\n", len(comp_slice), comp_slice[len(comp_slice)/2])
 }
